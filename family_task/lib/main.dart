f1ba01f3cd14528116a0696552285b0b75f6a1ca
matartiflette <maxence.tardivel.mt@gmail.com>
2025-08-23 19:55:45 +0200

    oeoeo

--- family_task/lib/main.dart
+++ family_task/lib/main.dart
@@ -27,31 +27,47 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+    // ↓ Add this.
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+
 }
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;                 // ← Add this.
 
-    
     return Scaffold(
       body: Column(
         children: [
           Text('A random AWESOME idea:'),
-          Text(appState.current.asLowerCase),
-
-          // ↓ Add this.
+          BigCard(pair: pair),                // ← Change to this.
           ElevatedButton(
             onPressed: () {
-              print('button pressed!');
+              appState.getNext();
             },
             child: Text('Next'),
           ),
-
         ],
       ),
     );
+  }
+}
 
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(pair.asLowerCase);
   }
 }
